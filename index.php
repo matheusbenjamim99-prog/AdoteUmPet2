c422eb7cea63b2ea379d6998b80a3fca4923aa61
matheusbenjamim99-prog <matheusbenjamim99@gmail.com>
2026-03-30 17:55:08 -0300

    Melhoria nas urls

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
-<?php include("config.php"); 
-include("includes/header.php");
-?>
-    <h1>🐶 Sistema de Adoção de Animais</h1>
-    <p>Este site ajuda animais a encontrarem um novo lar cheio de amor ❤️</p>
+<?php
+$url = $_GET['url'] ?? 'home';
 
-    <a href="login.php">Login</a> |
-    <a href="cadastro.php">Cadastrar</a> |
-    <a href="listar_An.php">Ver animais</a>
-<?php include("includes/footer.php");?>
+switch ($url) {
+    case 'home':
+        include 'pages/home.php';
+        break;
+
+    case 'login':
+        include 'pages/login.php';
+        break;
+
+    case 'cadastro':
+        include 'pages/cadastro.php';
+        break;
+
+    case 'animais':
+        include 'pages/listar_animais.php';
+        break;
+
+    case 'animais/cadastrar':
+        include 'pages/cadastrar_animal.php';
+        break;
+
+    default:
+        echo "Página não encontrada";
+}
